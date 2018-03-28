5137fcbed6d7e47a00eb9dba8a54e39707cfcfe6
Prateek Bhatia <prateek.bhatia@ixigo.com>
2018-03-28 14:00:21 -0700

    Added sponsor tab to slicknav

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,11 +9,11 @@ $(window).scroll(function() {
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    // $(document).on('click', 'a.page-scroll', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: $($anchor.attr('href')).offset().top
+    //     }, 1500, 'easeInOutExpo');
+    //     event.preventDefault();
+    // });
 });
